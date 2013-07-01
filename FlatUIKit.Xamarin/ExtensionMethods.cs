fccfc65ded3d6884166470c22aaa9f88c9dce498
Heath Hopkins <dynamicdna@gmail.com>
2013-07-01 11:29:54 -0500

    Add Lighten and Darken extensions to UIColor

--- FlatUIKit.Xamarin/ExtensionMethods.cs
+++ FlatUIKit.Xamarin/ExtensionMethods.cs
@@ -24,5 +24,64 @@ namespace FlatUIKit
         {
             return new NSString(label.Text).StringSize(label.Font);
         }
+
+        /// <summary>
+        /// Returns a lighter color
+        /// </summary>
+        /// <param name="steps">The number of steps to lighten.</param>
+        public static UIColor Lighten(this UIColor color, int steps)
+        {
+            int modifier = 16 * steps;
+
+            float rF, gF, bF, aF;
+            color.GetRGBA(out rF, out gF, out bF, out aF);
+
+            int r, g, b, a;
+            r = (int)Math.Ceiling(rF * 255);
+            g = (int)Math.Ceiling(gF * 255);
+            b = (int)Math.Ceiling(bF * 255);
+            a = (int)Math.Ceiling(aF * 255);
+
+            r += modifier;
+            g += modifier;
+            b += modifier;
+            // leave 'a' alone?
+
+            r = r > 255 ? 255 : r;
+            g = g > 255 ? 255 : g;
+            b = b > 255 ? 255 : b;
+
+            return UIColor.FromRGBA(r, g, b, a);
+        }
+
+        /// <summary>
+        /// Returns a darker color
+        /// </summary>
+        /// <param name="steps">The number of steps to darken.</param>
+        public static UIColor Darken(this UIColor color, int steps)
+        {
+            int modifier = 16 * steps;
+
+            float rF, gF, bF, aF;
+
+            color.GetRGBA(out rF, out gF, out bF, out aF);
+
+            int r, g, b, a;
+            r = (int)Math.Ceiling(rF * 255);
+            g = (int)Math.Ceiling(gF * 255);
+            b = (int)Math.Ceiling(bF * 255);
+            a = (int)Math.Ceiling(aF * 255);
+
+            r -= modifier;
+            g -= modifier;
+            b -= modifier;
+            // leave 'a' alone?
+
+            r = r < 0 ? 0 : r;
+            g = g < 0 ? 0 : g;
+            b = b < 0 ? 0 : b;
+
+            return UIColor.FromRGBA(r, g, b, a);
+        }
     }
 }
