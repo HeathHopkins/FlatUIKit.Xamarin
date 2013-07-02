7aa0c89bd9c6142b360de3dd48cfb8a3f6b56f33
Heath Hopkins <dynamicdna@gmail.com>
2013-07-02 10:42:18 -0500

    add short hex string support to ToUIColor

--- FlatUIKit.Xamarin/ExtensionMethods.cs
+++ FlatUIKit.Xamarin/ExtensionMethods.cs
@@ -9,9 +9,13 @@ namespace FlatUIKit
     {
         public static UIColor ToUIColor(this string hexString)
         {
-            if (hexString.Contains("#")) hexString = hexString.Replace("#", "");
+            hexString = hexString.Replace("#", "");
 
-            if (hexString.Length != 6) return UIColor.Red;
+            if (hexString.Length == 3)
+                hexString = hexString + hexString;
+
+            if (hexString.Length != 6)
+                throw new Exception("Invalid hex string");
 
             int red = Int32.Parse(hexString.Substring(0,2), System.Globalization.NumberStyles.AllowHexSpecifier);
             int green = Int32.Parse(hexString.Substring(2,2), System.Globalization.NumberStyles.AllowHexSpecifier);
